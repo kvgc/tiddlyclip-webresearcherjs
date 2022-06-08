d77a742f660025b1cc6cd9be9d860441cbd42901
keerthi <kvch@ucdavis.edu>
2022-06-07 20:47:14 -0700

    fixing iframe loading issues

--- extension/content/webresearcher/popup.js
+++ extension/content/webresearcher/popup.js
@@ -13,16 +13,17 @@ document.addEventListener('DOMContentLoaded', function() {
       iframeElement.src=currentTab;
     });
     refreshIframe.addEventListener('click', function() {
-      iframeElement.src=iframeElement.src;
+      document.getElementById('iframeDiv').innerHTML = `<iframe id="browser" src="`+ currentTab +`"style="position: absolute; height: 88%; width:100%;"> ></iframe>`;
     });
 });
 
 var currentTab = '';
 var urlExceptions = ['https://mail.google.com/mail/u/0/','https://web.whatsapp.com/'];
+
 function handleUpdated(tabId, changeInfo, tabInfo) {
   // currentTab = "http://0.0.0.0:8001/tiddlyroam3.html#"+tabInfo.url.replace(/(^\w+:|^)\/\//, '');
   if(!(tabInfo.url in urlExceptions)){
-    currentTab = "http://0.0.0.0:8001/tiddlyroam3.html#"+tabInfo.title;
+    currentTab = "http://0.0.0.0:8001/tiddlyroam3.html#"+"#:[search["+tabInfo.url+"]]";
 
     var link = document.getElementById('link');
     var iframeElement = document.getElementById('browser');
